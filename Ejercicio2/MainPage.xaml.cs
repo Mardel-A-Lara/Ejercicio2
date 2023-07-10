0e209e27dee6fff67e6b1b186772870d8b0bc1a2
Mardel Lara <alexanderchandy2018@gmail.com>
2023-07-09 21:56:18 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnsalvar_Clicked(object sender, EventArgs e)
+        {
+            // var mostrar = nombre.Text  + apellido.Text  + edad.Text  + correo.Text;
+
+            //DisplayAlert("Aviso", "sus datos son " + mostrar.ToString(), "OK");
+
+            // Views.PageResultado page = new Views.PageResultado();
+            // await Navigation.PushAsync(page);
+
+            var Datos = new Models.Datos
+            {
+                nombre= nombre.Text,
+                apellido=apellido.Text,
+                edad=   edad.Text,
+                correo= correo.Text,
+            };
+            Views.PageResultado page = new Views.PageResultado();
+            page.BindingContext= Datos;
+            await Navigation.PushAsync(page);
+
+
+        }
+    }
+}
---
+++ Ejercicio2/Models/Datos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2.Models
+{
+    public class Datos
+    {
+        public int id { get; set; }
+
+        public string nombre { get; set; }    
+        public string apellido { get; set; }
+        public string edad { get; set; }
+
+        public string correo { get; set; }
+
+
+    }
+}
